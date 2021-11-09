ebac7cc076b34ea295fa536e612232900b43e597
shruti <nakranishruti123@gmail.com>
2021-11-09 16:34:04 +0530

    refetch query by providing variables

--- src/components/useQueryExample.jsx
+++ src/components/useQueryExample.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import {
   useQuery,
-  gql
+  gql,
+  NetworkStatus
 } from "@apollo/client";
 
-export default function Simple({onUserSelected}) {
-  const { loading, error, data } = useQuery(gql`
- {
-    events {
-      name
-      _id
-      age
-    }
-  }
-  `);
+const query = gql`
+query login($email:String!,$password:String!){
+ login(email:$email,password:$password){
+   userId
+   token
+ }
+ }
+ `
+export default function Simple({email,password}) {
+  const { loading, error, data ,refetch,networkStatus} = useQuery(query,{variables: { email:"test4@test.com",password:"123456" },notifyOnNetworkStatusChange: true});
+  if (networkStatus === NetworkStatus.refetch) return 'Refetching!';
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(${error.message}</p>;
     return (
-      <select name="user" onChange={onUserSelected}>
-      {data.events.map(event => (
-        <option key={event._id} value={event.name}>
-        {event.name}
-      </option>
-      ))};
-      </select>
+      <>
+      <h5>userId {data.login.userId}</h5>
+      <button onClick={() => refetch({
+          email:"test43@test.com" ,password:"123456" // Always refetches a dalmatian instead of original breed
+        })}>Refetch!</button>
+      </>
     )
 }
 
